f2c9543ae4efd50da85e98cd94f67f1b42144c30
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2020-08-11 14:15:54 +0900

    Fix startup script to delete itself if failed.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- gcp/functions/neco.go
+++ gcp/functions/neco.go
@@ -46,33 +46,48 @@ func (b *NecoStartupScriptBuilder) WithNecoApps(branch string) (*NecoStartupScri
 
 // Build  builds startup script
 func (b *NecoStartupScriptBuilder) Build() string {
-	s := `#! /bin/sh`
+	s := `#! /bin/sh -x
 
-	s += `
+delete_myself()
+{
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+/snap/bin/gcloud --quiet compute instances delete $NAME --zone=$ZONE
+}
+
+prepare_scratch()
+{
 # mkfs and mount local SSD on /var/scratch
-mkfs -t ext4 -F /dev/disk/by-id/google-local-ssd-0
-mkdir -p /var/scratch
-mount -t ext4 /dev/disk/by-id/google-local-ssd-0 /var/scratch
+mkfs -t ext4 -F /dev/disk/by-id/google-local-ssd-0 &&
+mkdir -p /var/scratch &&
+mount -t ext4 /dev/disk/by-id/google-local-ssd-0 /var/scratch &&
 chmod 1777 /var/scratch
+}
+
+if ! prepare_scratch ; then delete_myself; fi
 `
 
 	if b.withFluentd {
 		s += `
+with_fluentd()
+{
 # Run fluentd to export syslog to Cloud Logging
-curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
-bash add-logging-agent-repo.sh
-apt-get update
-apt-cache madison google-fluentd
-apt-get install -y google-fluentd
-apt-get install -y google-fluentd-catch-all-config-structured
-service google-fluentd start
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh &&
+bash add-logging-agent-repo.sh &&
+apt-get update &&
+apt-cache madison google-fluentd &&
+apt-get install -y google-fluentd &&
+apt-get install -y google-fluentd-catch-all-config-structured &&
+service google-fluentd start &&
 # This line is needed to ensure that fluentd is running
 service google-fluentd restart
+}
+
+if ! with_fluentd ; then delete_myself; fi
 `
 	}
 
-	if len(b.necoBranch) > 0 {
-		s += fmt.Sprintf(`
+	s += fmt.Sprintf(`
 # Set environment variables
 HOME=/root
 GOPATH=${HOME}/go
@@ -82,26 +97,36 @@ NECO_DIR=${GOPATH}/src/github.com/cybozu-go/neco
 export HOME GOPATH GO111MODULE PATH NECO_DIR
 
 # Run neco
-mkdir -p ${GOPATH}/src/github.com/cybozu-go
-cd ${GOPATH}/src/github.com/cybozu-go
-git clone https://github.com/cybozu-go/neco
-cd ${GOPATH}/src/github.com/cybozu-go/neco/dctest
-git checkout %s
+run_neco()
+{
+mkdir -p ${GOPATH}/src/github.com/cybozu-go &&
+cd ${GOPATH}/src/github.com/cybozu-go &&
+git clone https://github.com/cybozu-go/neco &&
+cd ${GOPATH}/src/github.com/cybozu-go/neco/dctest &&
+git checkout %s &&
 make setup placemat MENU_ARG=menu-ss.yml && make test SUITE=bootstrap
+}
+
+if ! run_neco ; then delete_myself; fi
 `, b.necoBranch)
-	}
 
 	if len(b.necoAppsBranch) > 0 {
 		s += fmt.Sprintf(`
+run_necoapps()
+{
 # Run neco-apps
-cd ${GOPATH}/src/github.com/cybozu-go
-git clone https://github.com/cybozu-go/neco-apps
-cd ${GOPATH}/src/github.com/cybozu-go/neco-apps/test
-git checkout %s
-gcloud secrets versions access latest --secret="%s" > account.json
+cd ${GOPATH}/src/github.com/cybozu-go &&
+git clone https://github.com/cybozu-go/neco-apps &&
+cd ${GOPATH}/src/github.com/cybozu-go/neco-apps/test &&
+git checkout %s &&
+gcloud secrets versions access latest --secret="%s" > account.json &&
 make setup dctest BOOTSTRAP=1
+}
+
+if ! run_necoapps ; then delete_myself; fi
 `, b.necoAppsBranch, accountSecretName)
 	}
+
 	return s
 }
 
--- go.mod
+++ go.mod
@@ -5,22 +5,17 @@ go 1.13
 require (
 	cloud.google.com/go/pubsub v1.6.1
 	github.com/cybozu-go/log v1.6.0
+	github.com/cybozu-go/neco v0.0.0-20200811020525-e0b74ec941b3 // indirect
 	github.com/cybozu-go/well v1.10.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.3.3
 	github.com/nlopes/slack v0.6.0
-	github.com/pelletier/go-toml v1.3.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rakyll/statik v0.1.7
-	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cobra v1.0.0
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.7.1
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.30.0
 	google.golang.org/appengine v1.6.6
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
 	sigs.k8s.io/yaml v1.2.0
 )
