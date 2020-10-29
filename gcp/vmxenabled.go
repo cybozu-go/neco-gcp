945e2f923995f311fc054c9aedf051b47417a510
Masayuki Ishii <47380942+masa213f@users.noreply.github.com>
2020-10-29 15:23:39 +0900

    Revert "Add disable trasparent-hugepage service"

--- gcp/vmxenabled.go
+++ gcp/vmxenabled.go
@@ -33,7 +33,6 @@ var (
 		"/etc/containers/registries.conf",
 		"/etc/docker/daemon.json",
 		"/etc/profile.d/go.sh",
-		"/etc/systemd/system/disable-transparent-hugepage.service",
 		"/usr/local/bin/podenter",
 	}
 )
@@ -119,11 +118,6 @@ func SetupVMXEnabled(ctx context.Context, project string, option []string) error
 	}
 
 	err = dumpStaticFiles()
-	if err != nil {
-		return err
-	}
-
-	err = StartService(ctx, "disable-transparent-hugepage")
 	if err != nil {
 		return err
 	}
