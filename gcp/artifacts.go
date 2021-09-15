6b91f9c9e8ec4007807c7ca0309c80dd450bd5ee
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2021-09-15 11:28:15 +0900

    update golang to 1.17
    Signed-off-by: UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>

--- gcp/artifacts.go
+++ gcp/artifacts.go
@@ -1,7 +1,7 @@
 package gcp
 
 var artifacts = artifactSet{
-	goVersion:        "1.16.2",
+	goVersion:        "1.17.1",
 	etcdVersion:      "3.3.19",
 	placematVersion:  "2.0.5",
 	coreOSVersion:    "2605.5.0",
