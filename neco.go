4ede6a85f15b589ea0bf232e3f084f5f80c8debb
Masayuki Ishii <47380942+masa213f@users.noreply.github.com>
2021-10-14 15:55:22 +0900

    Skip creating dockerconfig files (#81)
    * Skip creating dockerconfig files
    
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- neco.go
+++ neco.go
@@ -165,8 +165,6 @@ git clone https://github.com/cybozu-go/neco-apps &&
 cd ${GOPATH}/src/github.com/cybozu-go/neco-apps/test &&
 git checkout %s &&
 gcloud secrets versions access latest --secret="%s" > account.json &&
-gcloud secrets versions access latest --secret="%s" > ghcr_dockerconfig.json &&
-gcloud secrets versions access latest --secret="%s" > quay_dockerconfig.json &&
 make setup dctest SUITE=bootstrap OVERLAY=neco-dev
 }
 
@@ -175,7 +173,7 @@ if run_necoapps ; then
 else
   delete_myself
 fi
-`, b.necoAppsBranch, necoAppsAccountSecretName, ghcrDockerConfigName, quayDockerConfigName)
+`, b.necoAppsBranch, necoAppsAccountSecretName)
 	}
 	return s
 }
