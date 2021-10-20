bed6162cabfd8e00cd3dd31d6850c646b2c483c9
Masayuki Ishii <47380942+masa213f@users.noreply.github.com>
2021-10-20 10:53:08 +0900

    Update functions.go
    Co-authored-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- functions.go
+++ functions.go
@@ -7,11 +7,11 @@ import (
 	"github.com/cybozu-go/neco-gcp/pkg/slacknotifier"
 )
 
-// NOTE: A entrypoint of the Cloud Functions must be in the root of this project.
+// NOTE: An entrypoint of the Cloud Functions must be in the root of this project.
 // ref: https://cloud.google.com/functions/docs/writing#structuring_source_code
 // >> For the Go runtime, your function must be in a Go package at the root of your project. Your function cannot be in package main. Sub-packages are only supported when using Go modules.
 
-// SlackNotifierEntryPoint is a entrypoint for the slack-notifier.
+// SlackNotifierEntryPoint is the entrypoint for the slack-notifier.
 func SlackNotifierEntryPoint(ctx context.Context, m *pubsub.Message) error {
 	return slacknotifier.SendNotification(ctx, slackNotifierConfigName, m)
 }
