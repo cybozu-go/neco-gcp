9e3accc129f92137507075bd8bd44c110d0e081d
YZ775 <yuzuki-mimura@cybozu.co.jp>
2025-03-07 07:41:53 +0000

    update dependencies

--- config_vmxenabled.go
+++ config_vmxenabled.go
@@ -6,16 +6,16 @@ import "github.com/cybozu-go/neco-gcp/pkg/setup"
 // This setting is used by both "necogcp create-image" and "necogcp neco-test create-image".
 const (
 	VMXEnabledBaseImageProject = "ubuntu-os-cloud"
-	VMXEnabledBaseImage        = "ubuntu-2204-jammy-v20240701"
+	VMXEnabledBaseImage        = "ubuntu-2204-jammy-v20250305"
 )
 
 // The settings of software which installed in the VMXEnabled image.
 // This setting is used by both "necogcp create-image" and "necogcp neco-test create-image".
 var VMXEnabledArtifacts = setup.ArtifactSet{
-	GoVersion:       "1.23.2",
-	EtcdVersion:     "3.5.17",
-	PlacematVersion: "2.4.7",
-	CoreOSVersion:   "3975.2.2",
+	GoVersion:       "1.24.1",
+	EtcdVersion:     "3.5.19",
+	PlacematVersion: "2.4.8",
+	CoreOSVersion:   "4152.2.1",
 	CtVersion:       "0.9.3", //If upgrading a version, make sure the binary is included in the GitHub release
 	DebPackages: []string{
 		"git",
